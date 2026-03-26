297e0ca51ec89bc9b0f38c3c1cd3a5d88c69f7f3
hongquan23 <11a3luonghongquan2@gmail.com>
2026-03-26 18:44:31 +0700

    add socket method

--- backend/api/endpoints/event.py
+++ backend/api/endpoints/event.py
@@ -1,11 +1,22 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends,WebSocket,WebSocketDisconnect
 from sqlalchemy.orm import Session
 from db.database import get_db
 from schemas.event_schema import EventCreate
 from services.event_service import process_event
 from models.event import Event
+from services.websocket_manager import manager
 
 router = APIRouter()
+
+@router.websocket("/alerts")
+async def websocket_endpoint(websocket:WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+
 
 @router.post("/events")
 async def recieve_event(data: EventCreate,db: Session = Depends(get_db)):
@@ -24,6 +35,15 @@ async def recieve_event(data: EventCreate,db: Session = Depends(get_db)):
 
     # gửi thông báo (demo)
     if message:
-        print("NOTIFICATION:", message)
+        alert_payload = {
+            "event_type" : data.event_type,
+            "camera_id" : data.camera_id,
+            "timestamp" : str(data.timestamp)
+
+        }
+
+        await manager.broadcast(alert_payload)
 
     return {"status": "ok", "message": message}
+    
+
--- backend/services/event_service.py
+++ backend/services/event_service.py
@@ -2,7 +2,14 @@ def process_event(data):
     if data.confidence < 0.7:
         return None
     
-    if data.event_type == "fall_detected":
+    if data.event_type == "fall_detected":   #phát hiện té ngã
         return f"fall detected at {data.camera_id}"
     
+    if data.event_type == "violence_detected": #phát hiện bạo lực
+        return f"violence detected at {data.camera_id}"
+    
+    if data.event_type == "imobile_detected": #phát hiện bất động
+        return f"imobile detected at {data.camera_id}"
+    
     return f"Event {data.event_type} aats {data.camera_id}"
+
